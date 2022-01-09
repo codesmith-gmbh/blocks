6d9189d752fdfe9cf5069a76e249c742482e2474
Stanislas Nanchen <stan@codesmith.ch>
2022-01-09 11:46:18 +0100

    fix build.clj
    Signed-off-by: Stanislas Nanchen <stan@codesmith.ch>

--- build/build.clj
+++ build/build.clj
@@ -4,27 +4,28 @@
             [ch.codesmith.anvil.release :as rel]
             [clojure.tools.build.api :as b]))
 
-(def lib 'ch.codesmith/block)
+(def lib 'ch.codesmith/blocks)
 (def version (str "0.3." (b/git-count-revs {})))
 (def release-branch-name "master")
 
 
 (defn run-tests []
-  (sh/sh! "/bin/kaocha"))
+  (sh/sh! "./bin/kaocha"))
 
 (defn jar [_]
   (libs/jar {:lib        lib
              :version    version
              :target-dir "target"
              :with-pom?  true
-             :clean      true}))
+             :clean?     true}))
 
 (defn release [_]
   (rel/check-released-allowed release-branch-name)
   (run-tests)
   (let [jar-file (jar {})]
     (libs/deploy {:jar-file jar-file
-                  :lib      lib})
+                  :lib      lib
+                  :pom-file "target/classes/META-INF/maven/ch.codesmith/blocks/pom.xml"})
     (rel/git-release! {:deps/coord          lib
                        :version             version
                        :release-branch-name release-branch-name})))
