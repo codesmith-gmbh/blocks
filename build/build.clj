3d5a1382572bd0397c0952c07cf6e72ac0c47255
Stanislas Nanchen <stan@codesmith.ch>
2022-01-22 15:57:42 +0100

    Tooling
    Signed-off-by: Stanislas Nanchen <stan@codesmith.ch>

--- build/build.clj
+++ build/build.clj
@@ -8,16 +8,26 @@
 (def version (str "0.3." (b/git-count-revs {})))
 (def release-branch-name "master")
 
+(def description-data
+  {:license        :epl
+   :inception-year 2020
+   :description    "Utils on top of integrant"
+   :gh-project     "codesmith-gmbh/blocks"
+   :org-name       "Codesmith GmbH"
+   :authors        ["Stanislas Nanchen"]
+   :org-url        "https://codesmith.ch"})
+
 
 (defn run-tests []
   (sh/sh! "./bin/kaocha"))
 
 (defn jar [_]
-  (libs/jar {:lib        lib
-             :version    version
-             :target-dir "target"
-             :with-pom?  true
-             :clean?     true}))
+  (libs/jar {:lib              lib
+             :version          version
+             :target-dir       "target"
+             :with-pom?        true
+             :description-data description-data
+             :clean?           true}))
 
 (defn release [_]
   (rel/check-released-allowed release-branch-name)
@@ -26,9 +36,8 @@
     (libs/deploy {:jar-file jar-file
                   :lib      lib
                   :pom-file "target/classes/META-INF/maven/ch.codesmith/blocks/pom.xml"})
-    (sh/sh! "git" "clean" "-df")
     (rel/git-release! {:deps/coord          lib
                        :version             version
                        :release-branch-name release-branch-name
-                       :deps/manifest       :mvn})))
+                       :artifact-type       :mvn})))
 
